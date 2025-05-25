bce86a2a0689ba3b383405bcbc72677f24ef0f1b
Mahfujur Rahman <mahfujurrahmancityu@gmail.com>
2025-05-25 23:24:38 +0600

    footer and newsletter section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import AnnouncementBar from "./components/AnnouncementBar";
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import NewsletterSection from "./components/NewsletterSection";
 import ProductList from "./components/ProductList";
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
       <main className="container mx-auto px-4 md:px-8 py-8">
         <ProductList />
       </main>
+      <NewsletterSection />
+      <Footer />
     </>
   );
 }
